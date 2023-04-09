47ed8c4ec0285cd5ca2900683bf451856b65ce6e
Chari Altagracia <chari.altagracia1@axp.com>
2023-04-09 19:33:20 -0400

    hover over

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -1,12 +1,23 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Popover from "react-bootstrap/Popover";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 
 export default function SummaryForm() {
   const [tcChecked, setTcChecked] = useState(false);
+
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Body>No ice cream will actually be delivered</Popover.Body>
+    </Popover>
+  );
   const checkboxLabel = (
     <span>
-      I agree to <span style={{ color: "blue" }}> Tearms and Consitons</span>
+      I agree to
+      <OverlayTrigger placement="right" overlay={popover}>
+        <span style={{ color: "blue" }}> Terms and Conditions/i</span>
+      </OverlayTrigger>
     </span>
   );
 
--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,10 +1,11 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import SummaryForm from "../SummaryForm";
+import userEvent from "@testing-library/user-event";
 
 test("initilal condition", () => {
   render(<SummaryForm />);
   const checkbox = screen.getByRole("checkbox", {
-    name: /I agree to Tearms and Consitons/i,
+    name: /I agree to Terms and Conditions/i,
   });
   expect(checkbox).not.toBeChecked();
 
@@ -12,17 +13,39 @@ test("initilal condition", () => {
   expect(button).toBeDisabled();
 });
 
-test("checkbox enables button on first click and disable on second click", () => {
+test("checkbox enables button on first click and disable on second click", async () => {
+  const user = userEvent.setup();
   render(<SummaryForm />);
   const checkbox = screen.getByRole("checkbox", {
-    name: /I agree to Tearms and Consitons/i,
+    name: /I agree to Terms and Conditions/i,
   });
   const button = screen.getByRole("button", { name: /confirm order/i });
 
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(button).toBeEnabled();
 
   //disbale button by uncheck checkbox
-  fireEvent.click(checkbox);
+  await user.click(checkbox);
   expect(button).toBeDisabled();
+});
+
+test("popever responds to hover", async () => {
+  const user = userEvent.setup();
+  render(<SummaryForm />);
+
+  // popover start out hidden
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delived/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
+
+  // popover appears on mouseover of checkbox label
+  const termsandConditions = screen.getByText(/terms and conditions/i);
+  await user.hover(termsandConditions);
+  const popOver = screen.getByText(/no ice cream will actually be delivered/i);
+  expect(popOver).toBeInTheDocument();
+
+  //popover disappears when we mouse out
+  await user.unhover(termsandConditions);
+  expect(popOver).not.toBeInTheDocument();
 });
